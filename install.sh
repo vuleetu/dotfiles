856b142316b21e4358fa6f7a41da84b52e6b893f
Fisher <fisher@yun.io>
2013-08-20 14:31:54 +0800

    fix bug

--- install.sh
+++ install.sh
@@ -142,7 +142,7 @@ fi
 
 if [ ! -d ~/.autojump ]; then
     echo "Installing autojump"
-    git clone git://github.com/joelthelion/.autojump.git ~/.autojump
+    git clone git://github.com/joelthelion/autojump.git ~/.autojump
     cd ~/.autojump && ./install.sh
 fi
 
