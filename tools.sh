772bc8d2419ad9bc8325d4e6014e54ad1898b4e0
Fisher <fisher@yun.io>
2013-09-09 18:21:12 +0800

    update

--- tools.sh
+++ tools.sh
@@ -34,4 +34,101 @@ if ! command -v http > /dev/null 2>&1; then
     esac
 fi
 
+if ! command -v dstat > /dev/null 2>&1; then
+    echo -n "Install dstat, a combine of iostat, vmstat and ifstat(y/N)?"
+    read dyn
+    case $dyn in
+        [Yy]* )
+            echo "Installing dstat"
+            sudo apt-get install dstat
+            echo 'alias dstat="dstat -cdlmnpsy"' >> ~/.zshrc
+            ;;
+        * ) ;;
+    esac
+fi
+
+if ! command -v slurm > /dev/null 2>&1; then
+    echo -n "Install slurm(y/N)?"
+    read dyn
+    case $dyn in
+        [Yy]* )
+            echo "Installing slurm"
+            sudo apt-get install slurm
+            ;;
+        * ) ;;
+    esac
+fi
+
+if ! command -v htop > /dev/null 2>&1; then
+    echo -n "Install htop, ncursed-based process viewer similar to top(y/N)?"
+    read dyn
+    case $dyn in
+        [Yy]* )
+            echo "Installing htop"
+            sudo apt-get install htop
+            ;;
+        * ) ;;
+    esac
+fi
+
+if ! command -v atop > /dev/null 2>&1; then
+    echo -n "Install atop(y/N)?"
+    read dyn
+    case $dyn in
+        [Yy]* )
+            echo "Installing atop"
+            sudo apt-get install atop
+            ;;
+        * ) ;;
+    esac
+fi
+
+if ! command -v iotop > /dev/null 2>&1; then
+    echo -n "Install iotop(y/N)?"
+    read dyn
+    case $dyn in
+        [Yy]* )
+            echo "Installing iotop"
+            sudo apt-get install iotop
+            ;;
+        * ) ;;
+    esac
+fi
+
+if ! command -v newsbeuter > /dev/null 2>&1; then
+    echo -n "Install newsbeuter, rss reader(y/N)?"
+    read dyn
+    case $dyn in
+        [Yy]* )
+            echo "Installing newsbeuter"
+            sudo apt-get install newsbeuter
+            ;;
+        * ) ;;
+    esac
+fi
+
+if ! command -v iftop > /dev/null 2>&1; then
+    echo -n "Install iftop(y/N)?"
+    read dyn
+    case $dyn in
+        [Yy]* )
+            echo "Installing iftop"
+            sudo apt-get install iftop
+            ;;
+        * ) ;;
+    esac
+fi
+
+if ! command -v ethstatus > /dev/null 2>&1; then
+    echo -n "Install ethstatus(y/N)?"
+    read dyn
+    case $dyn in
+        [Yy]* )
+            echo "Installing ethstatus"
+            sudo apt-get install ethstatus
+            ;;
+        * ) ;;
+    esac
+fi
+
 echo "Done"
