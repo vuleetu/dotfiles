bc16feefc4a971d726720c4d8d47a8e71828244f
fisher <fisher@yunio.com>
2018-01-11 17:56:01 +0800

    Optimize bootstrap script

--- res/bootstrap.zsh
+++ res/bootstrap.zsh
@@ -2,7 +2,8 @@ BASE=`python -c 'import os,sys;print(os.path.realpath(sys.argv[1]))' $0`
 RES_DIR=`dirname $BASE`
 ENV_DIR=`dirname $RES_DIR`/env
 
-for s in $ENV_DIR/*; do
+#Load all env script
+for s in $ENV_DIR/*.env; do
   source $s
 done
 
